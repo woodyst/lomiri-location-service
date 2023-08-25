woodyst/lomiri-location-service

commit 13473aa70d683ef073685181e48596a1f1374baa

gps: Re-register callbacks in start_positioning This allows GPS to be used after running Waydroid (which overwrites our callbacks) and after the GNSS HAL restarts in Halium.

src/location_service/com/lomiri/location/providers/gps/android_hardware_abstraction_layer.cpp
@@ -473,6 +473,10 @@ bool android::HardwareAbstractionLayer::is_capable_of(gps::Capability capability
 bool android::HardwareAbstractionLayer::start_positioning()
 {
     VLOG(1) << __PRETTY_FUNCTION__ << ": " << this << ", " << impl.gps_handle;
+
+    // Re-register callbacks in case they have been overwritten (e.g. by Waydroid)
+    impl.register_callbacks();
+
     return u_hardware_gps_start(impl.gps_handle);
 }
 
@@ -576,6 +580,14 @@ android::HardwareAbstractionLayer::Impl::Impl(
     gps_params.request_refloc_cb = HardwareAbstractionLayer::on_ril_request_reference_location;
 
     gps_params.context = parent;
+
+    register_callbacks();
+}
+
+void android::HardwareAbstractionLayer::Impl::register_callbacks()
+{
+    if (gps_handle)
+        u_hardware_gps_delete(gps_handle);
 
     gps_handle = u_hardware_gps_new(std::addressof(gps_params));
 

src/location_service/com/lomiri/location/providers/gps/android_hardware_abstraction_layer.h
@@ -254,6 +254,9 @@ struct HardwareAbstractionLayer : public gps::HardwareAbstractionLayer
         // Bootstraps access to the GPS chipset, wiring up all callbacks.
         Impl(android::HardwareAbstractionLayer* parent, const android::HardwareAbstractionLayer::Configuration& configuration);
 
+        // Wires up all callbacks, (re)gaining access to the GPS chipset.
+        void register_callbacks();
+
         // Adjusts the assistance and positioning mode in one go, returning false in case of issues.
         bool dispatch_updated_modes_to_driver();
 

commit 666fbef85829b4601c1007d2ff4d755539127c21

gps: fix sproradic crash due to uninitialized memory
Commit 13473aa70d68 ("gps: Re-register callbacks in start_positioning")
make HAL::Impl::register_callbacks() check if gps_handle exists so that
it can delete it. But because the constructor doesn't initialize that
variable, the check always succeed when called the first time in the
constructor, leading to calling free() with invalid memory.

Initialize that value first with nullptr, so that the first call doesn't
delete the uninitialized handle.

Fixes: 13473aa70d68 ("gps: Re-register callbacks in start_positioning")
Fixes: https://gitlab.com/ubports/development/core/location-service/-/issues/16

src/location_service/com/lomiri/location/providers/gps/android_hardware_abstraction_layer.cpp
@@ -556,7 +556,8 @@ bool android::HardwareAbstractionLayer::inject_reference_time(const ReferenceTim
 android::HardwareAbstractionLayer::Impl::Impl(
         android::HardwareAbstractionLayer* parent,
         const android::HardwareAbstractionLayer::Configuration& configuration)
-    : capabilities(0),
+    : gps_handle(nullptr),
+      capabilities(0),
       assistance_mode(gps::AssistanceMode::mobile_station_based),
       position_mode(gps::PositionMode::periodic),
       supl_assistant(*parent),